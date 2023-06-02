3014d258fe1974fb7a749eab061872f1d2638095
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-02 15:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateTextAfterDelay() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Example usage
+const outputElement = document.getElementById("output");
+
+updateTextAfterDelay().then((message) => {
+  outputElement.textContent = message;
+});
